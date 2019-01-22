83b7b427d382ecbe302ba2c59e023264049797f4
Andrew Balzer <andrewdbalzer@gmail.com>
2019-01-21 21:38:50 -0600

    initial commit

---
+++ src/components/Button.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+
+class Button extends React.Component {
+  static contextType = LanguageContext; // HAS to be called contextType
+
+  render() {
+    const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
+    return (
+      <button className="ui button primary">{text}</button>
+    );
+  }
+}
+
+export default Button;
